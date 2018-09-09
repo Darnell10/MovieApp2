8aaef1dd1357d66b0dd72f9c6f8e0d58b9cb38b2
Darnell O <DarnellOtto@ac.c4q.nyc>
2018-09-09 00:56:07 -0400

    presentation package and adapter class complete

--- app/src/main/java/com/example/d/movieapp2/presenation/Adapter.java
+++
@@ -1,4 +0,0 @@
-package com.example.d.movieapp2.presenation;
-
-public class Adapter {
-}
--- app/src/main/java/com/example/d/movieapp2/ui/SearchActivity.java
+++ app/src/main/java/com/example/d/movieapp2/ui/SearchActivity.java
@@ -3,6 +3,8 @@ package com.example.d.movieapp2.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.d.movieapp2.R;
+
 public class SearchActivity extends AppCompatActivity {
 
     @Override
