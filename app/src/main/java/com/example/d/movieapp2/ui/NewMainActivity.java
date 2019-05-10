52f453d00b854afff69c492d73fad6d8f5129c56
Darnell O <DarnellOtto@ac.c4q.nyc>
2019-05-10 03:12:26 -0400

    Update gradle + Dependancies, Add new activity. Cleanup some code, retrofit response setup

--- app/src/main/java/com/example/d/movieapp2/ui/NewMainActivity.java
+++ app/src/main/java/com/example/d/movieapp2/ui/NewMainActivity.java
@@ -4,15 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.d.movieapp2.R;
+import com.example.d.movieapp2.networking.MovieService;
 
 public class NewMainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_main);
     }
-
 
 
 }
