ac810f6cf5845771a1a651efed7c2a781023f848
Darnell10 <DarnellOtto@ac.c4q.nyc>
2020-04-18 01:02:08 -0400

    create Util file for handle images

---
+++ app/src/main/java/com/example/d/movieapp2/util/Util.kt
@@ -0,0 +1,32 @@
+package com.example.d.movieapp2.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.d.movieapp2.R
+
+fun getProgressDrawable(context: Context): CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 10f
+        centerRadius = 50f
+        start()
+    }
+}
+
+fun ImageView.loadImage(url: String?, progressDrawable: CircularProgressDrawable) {
+    val options: RequestOptions = RequestOptions()
+            .placeholder(progressDrawable)
+            .error(R.mipmap.ic_launcher)
+    Glide.with(context)
+            .setDefaultRequestOptions(options)
+            .load(url)
+            .into(this)
+}
+
+@BindingAdapter("android:imageUrl")
+fun loadImage(view: ImageView, url: String) {
+    view.loadImage(url, getProgressDrawable(view.context))
+}
--- app/src/main/java/com/example/d/movieapp2/view/MovieAdapter.kt
+++ app/src/main/java/com/example/d/movieapp2/view/MovieAdapter.kt
@@ -1,6 +1,10 @@
 package com.example.d.movieapp2.view
 
+import androidx.appcompat.view.menu.MenuView
 import com.example.d.movieapp2.models.MovieModel
 
 class MovieAdapter(private val movieList:ArrayList<MovieModel>) {
+
+
+
 }
