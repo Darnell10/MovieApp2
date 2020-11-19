be5d88f9940202266f3a0e8a5ee9a91b68f80011
Darnell10 <DarnellOtto@ac.c4q.nyc>
2020-11-18 20:27:08 -0500

    add companion object to MovieDataBase

--- app/src/main/java/com/example/d/movieapp2/models/MovieDataBase.kt
+++ app/src/main/java/com/example/d/movieapp2/models/MovieDataBase.kt
@@ -4,19 +4,28 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import java.util.concurrent.locks.Lock
 
 @Database(entities = arrayOf(MovieModel::class), version = 1, exportSchema = false)
 abstract class MovieDataBase : RoomDatabase() {
     abstract fun movieDao(): MovieDao
 
+    companion object {
+        @Volatile
+        private var instance: MovieDataBase? = null
+        private val Lock = Any()
 
+        operator fun invoke(context: Context) = instance ?: synchronized(Lock) {
+            instance ?: buildDataBase(context).also {
+                instance = it
+            }
+        }
 
-
-    private fun buildDataBase(context: Context) = Room.databaseBuilder(
-            context.applicationContext,
-            MovieDataBase::class.java,
-            "moviedatabase"
-    ).build()
-
+        private fun buildDataBase(context: Context) = Room.databaseBuilder(
+                context.applicationContext,
+                MovieDataBase::class.java,
+                "moviedatabase"
+        ).build()
+    }
 
 }
